fe48d5fc5f860b1e815352eb113234bb67fee8ce
v4d1kos <v4dikos@yandex.ru>
2022-10-11 15:17:29 +0700

    Начальная настройка бокового меню

--- app/src/main/java/com/example/calculator/MainActivity.java
+++ app/src/main/java/com/example/calculator/MainActivity.java
@@ -1,8 +1,13 @@
 package com.example.calculator;
 
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.drawerlayout.widget.DrawerLayout;
 
+import android.app.ActionBar;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +15,22 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Установка бокового меню
+        DrawerLayout drawerLayout = findViewById(R.id.drawer_layout);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this,
+                drawerLayout,
+                toolbar,
+                R.string.app_name,
+                R.string.app_name
+        );
+
+        drawerLayout.addDrawerListener(toggle);
+        toggle.syncState();
+
+
     }
 }
